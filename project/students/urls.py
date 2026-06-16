4dfff240375d0c38bfd4f1e408f9ad281798de72
hkrns523-creator <hkrns523@gmail.com>
2026-06-16 07:40:09 +0530

    Done changes

---
+++ project/students/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('add/', views.add_student, name='add_student'),
+    path('update/<int:id>/', views.update_student, name='update_student'),
+    path('delete/<int:id>/', views.delete_student, name='delete_student'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
